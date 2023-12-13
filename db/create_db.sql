02dc2ccbae2724d37981b51b154c86145303535a
yjgtbujyg <P0533105452@gmail.com>
2023-12-13 15:53:40 +0200

    ww

--- db/create_db.sql
+++ db/create_db.sql
@@ -1,57 +1,57 @@
+
 drop table if exists `tags`;
 CREATE TABLE `tags`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `name` VARCHAR(255) NOT NULL,
-    `status` TINYINT NOT NULL,
-	PRIMARY KEY `tags_id_primary`(`id`)
+    `status` TINYINT NOT NULL
 );
 
 drop table if exists `sites_tags`;
 CREATE TABLE `sites_tags`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `id_tags` BIGINT NOT NULL,
-    `site_id` BIGINT NOT NULL,
-    `status` TINYINT NOT NULL,
-    PRIMARY KEY `sites_tags_id_primary`(`id`)
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `tags_id` BIGINT NOT NULL,
+    `sites_id` BIGINT NOT NULL,
+    `status` TINYINT NOT NULL
 );
 
-
-CREATE TABLE `users_to_tags`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+drop table if exists `users_tags`;
+CREATE TABLE `users_tags`(
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `user_id` BIGINT NOT NULL,
-    `id_tag` BIGINT NOT NULL,
-    `status` TINYINT(1) NOT NULL
+    `tag_id` BIGINT NOT NULL,
+    `status` TINYINT NOT NULL
 );
-ALTER TABLE
-    `users_to_tags` ADD PRIMARY KEY `users_to_tags_id_primary`(`id`);
+drop table if exists `sites`;
+
 CREATE TABLE `sites`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `url` VARCHAR(255) NOT NULL,
-    `status` TINYINT(1) NOT NULL
+    `status` TINYINT NOT NULL
 );
-ALTER TABLE
-    `sites` ADD PRIMARY KEY `sites_id_primary`(`id`);
+drop table if exists `users`;
+
 CREATE TABLE `users`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `name` VARCHAR(255) NOT NULL,
     `email` VARCHAR(255) NOT NULL,
     `password` BIGINT NOT NULL
 );
-ALTER TABLE
-    `users` ADD PRIMARY KEY `users_id_primary`(`id`);
+drop table if exists `users_sites`;
+
 CREATE TABLE `users_sites`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `user_id` BIGINT NOT NULL,
     `site_id` BIGINT NOT NULL,
-    `status` TINYINT(1) NOT NULL
+    `status` TINYINT NOT NULL
 );
 ALTER TABLE
-    `users_sites` ADD PRIMARY KEY `users_sites_id_primary`(`id`);
-
+    `sites_tags` ADD CONSTRAINT `sites_tags_sites_id_foreign` FOREIGN KEY(`sites_id`) REFERENCES `sites`(`id`);
 ALTER TABLE
-    `sites_tags` ADD CONSTRAINT `sites_tags_site_id_foreign` FOREIGN KEY(`site_id`) REFERENCES `sites`(`id`);
+    `users_tags` ADD CONSTRAINT `users_tags_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `users`(`id`);
 ALTER TABLE
-    `sites_tags` ADD CONSTRAINT `sites_tags_id_tags_foreign` FOREIGN KEY(`id_tags`) REFERENCES `tags`(`id`);
+    `users_tags` ADD CONSTRAINT `users_tags_tag_id_foreign` FOREIGN KEY(`tag_id`) REFERENCES `tags`(`id`);
+ALTER TABLE
+    `sites_tags` ADD CONSTRAINT `sites_tags_tags_id_foreign` FOREIGN KEY(`tags_id`) REFERENCES `tags`(`id`);
 ALTER TABLE
     `users_sites` ADD CONSTRAINT `users_sites_site_id_foreign` FOREIGN KEY(`site_id`) REFERENCES `sites`(`id`);
 ALTER TABLE
